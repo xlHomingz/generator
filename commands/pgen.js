14de1967892319227533a3cb08958ce4d0697b0c
xfsfree <162906810+xfsfree@users.noreply.github.com>
2024-03-16 01:21:07 +0400

    Update pgen.js

--- commands/pgen.js
+++ commands/pgen.js
@@ -1,19 +1,19 @@
 // Dependencies
-const { MessageEmbed, Message } = require('discord.js');
+const { MessageEmbed } = require('discord.js');
 const fs = require('fs');
 const config = require('../config.json');
 const CatLoggr = require('cat-loggr');
-//Made By ScienceGear#4409 Youtube https://www.youtube.com/c/ScienceGearYT
+
 // Functions
 const log = new CatLoggr();
-const generated = new Set();
+const cooldowns = new Map(); // Cooldown sürelerini saklamak için bir Map
 
 module.exports = {
-  name: 'pgen', // Command name
-  description: 'Generate a specified service if stocked (premium).', // Command description
+  name: 'pgen',
+  description: 'Generate a specified service if stocked',
 
   /**
-   * Command exetute
+   * Command execute
    * @param {Message} message The message sent by user
    * @param {Array[]} args Arguments splitted by spaces after the command name
    */
@@ -22,142 +22,131 @@ module.exports = {
     try {
       message.client.channels.cache.get(config.pgenChannel).id; // Try to get the channel's id
     } catch (error) {
-      if (error) log.error(error); // If an error occured log to console
+      if (error) log.error(error);
 
-      // Send error messsage if the "error_message" field is "true" in the configuration
       if (config.command.error_message === true) {
         return message.channel.send(
           new MessageEmbed()
             .setColor(config.color.red)
-            .setTitle('Error occured!')
-            .setDescription('Premium generator is unavailable for now.')
+            .setTitle('Error occurred!')
+            .setDescription('Not a valid gen channel specified!')
             .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
             .setTimestamp()
         );
       } else return;
-    };
-    //Made By ScienceGear#4409 Youtube https://www.youtube.com/c/ScienceGearYT
-    // If the message channel id is the generator channel id in configuration
+    }
+
     if (message.channel.id === config.pgenChannel) {
-      // If the user have cooldown on the command
-      if (generated.has(message.author.id)) {
+      const cooldownTime = cooldowns.get(message.author.id) || 0;
+      const remainingTime = Math.max(0, cooldownTime - Date.now());
+
+      if (remainingTime > 0) {
         return message.channel.send(
           new MessageEmbed()
             .setColor(config.color.red)
             .setTitle('Cooldown!')
-            .setDescription(`Please wait **${config.genCooldownsec}** seconds before executing that command again!`)
+            .setDescription(`You are on cooldown. Remaining time: **${Math.floor(remainingTime / 60000)} minutes and ${Math.floor((remainingTime % 60000) / 1000)} seconds.**`)
             .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
             .setTimestamp()
         );
-      } else {
-        // Parameters
-        const service = args[0];
-        //Made By ScienceGear#4409 Youtube https://www.youtube.com/c/ScienceGearYT
-        // If the "service" parameter is missing
-        if (!service) {
-          return message.channel.send(
-            new MessageEmbed()
-              .setColor(config.color.red)
-              .setTitle('Missing parameters!')
-              .setDescription('Please specify a service name!')
-              .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
-              .setTimestamp()
-          );
-        };
+      }
 
-        // File path to find the given service
-        const filePath = `${__dirname}/../pstock/${args[0]}.txt`;
+      const service = args[0];
 
-        // Read the service file
-        fs.readFile(filePath, function(error, data) {
-          // If no error
-          if (!error) {
-            data = data.toString(); // Stringify the content
-            //Made By ScienceGear#4409 Youtube https://www.youtube.com/c/ScienceGearYT
-            const position = data.toString().indexOf('\n'); // Get position
-            const firstLine = data.split('\n')[0]; // Get the first line
+      if (!service) {
+        return message.channel.send(
+          new MessageEmbed()
+            .setColor(config.color.red)
+            .setTitle('Missing parameters!')
+            .setDescription('Please specify a service name!')
+            .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
+            .setTimestamp()
+        );
+      }
 
-            // If the service file is empty
-            if (position === -1) {
-              return message.channel.send(
-                new MessageEmbed()
-                  .setColor(config.color.red)
-                  .setTitle('Generator error!')
-                  .setDescription(`We don\'t have \`${args[0]}\` accounts currently!`)
-                  .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
-                  .setTimestamp()
-              );
-            };
-            //Made By ScienceGear#4409 Youtube https://www.youtube.com/c/ScienceGearYT
-            // Send messages to the user
-            message.author.send(
-              new MessageEmbed()
-                .setColor(config.color.green)
-                .setTitle('Generated account')
-                .addField('Service', `\`\`\`${args[0][0].toUpperCase()}${args[0].slice(1).toLowerCase()}\`\`\``, true)
-                .addField('account', `\`\`\`${firstLine}\`\`\``, true)
-                .setTimestamp()
-            )
+      const filePath = `${__dirname}/../pstock/${args[0]}.txt`;
 
-            // Send message to the channel if the user recieved the message
-            if (position !== -1) {
-              data = data.substr(position + 1); // Remove the gernerated account line
+      fs.readFile(filePath, function(error, data) {
+        if (!error) {
+          data = data.toString();
+          const position = data.toString().indexOf('\n');
+          const firstLine = data.split('\n')[0];
 
-              // Write changes
-              fs.writeFile(filePath, data, function(error) {
-                message.channel.send(
-                  new MessageEmbed()
-                    .setColor(config.color.green)
-                    .setTitle('account generated successfully!')
-                    .setDescription(`Check your private ${message.author}! *If you did not receive the message, please unlock your private!*`)
-                    .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
-                    .setTimestamp()
-                );
-
-                generated.add(message.author.id); // Add user to the cooldown set
-
-                // Set cooldown time
-                setTimeout(() => {
-                  generated.delete(message.author.id); // Remove the user from the cooldown set after expire
-                }, config.genCooldown);
-                //Made By ScienceGear#4409 Youtube https://www.youtube.com/c/ScienceGearYT
-                if (error) return log.error(error); // If an error occured, log to console
-              });
-            } else {
-              // If the service is empty
-              return message.channel.send(
-                new MessageEmbed()
-                  .setColor(config.color.red)
-                  .setTitle('Generator error!')
-                  .setDescription(`The \`${args[0]}\` service is empty!`)
-                  .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
-                  .setTimestamp()
-              );
-            };
-          } else {
-            // If the service does not exists
+          if (position === -1) {
             return message.channel.send(
               new MessageEmbed()
                 .setColor(config.color.red)
                 .setTitle('Generator error!')
-                .setDescription(`There is no \`${args[0]}\` service! Make sure you wrote it correctly!`)
+                .setDescription(`We don't have \`${args[0]}\` currently!`)
                 .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
                 .setTimestamp()
             );
-          };
-        });
-      };
+          }
+
+          const embedMessage = new MessageEmbed()
+            .setColor(config.color.green)
+            .setTitle('Generated service')
+            .addField('Service', `\`\`\`${args[0][0].toUpperCase()}${args[0].slice(1).toLowerCase()}\`\`\``, true)
+            .addField('‎ ', `\`\`\`${firstLine}\`\`\``, true)
+            .setTimestamp();
+
+          message.author.send(embedMessage);
+
+          if (position !== -1) {
+            data = data.substr(position + 1);
+
+            fs.writeFile(filePath, data, function(error) {
+              message.channel.send(
+                new MessageEmbed()
+                  .setColor(config.color.green)
+                  .setTitle('Generated successfully!')
+                  .setDescription(`Check your private ${message.author}! *If you did not receive the message, please unlock your private!*`)
+                  .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
+                  .setTimestamp()
+              );
+
+              cooldowns.set(message.author.id, Date.now() + 900000); // 10 dakika = 600000 milisaniye
+
+              setTimeout(() => {
+                cooldowns.delete(message.author.id);
+
+                // Cooldown süresi bittiğinde belirli bir kanala mesaj gönder
+                const cooldownChannel = message.client.channels.cache.get(config.pgenCooldownChannel);
+              
+              }, 900000);
+
+              if (error) return log.error(error);
+            });
+          } else {
+            return message.channel.send(
+              new MessageEmbed()
+                .setColor(config.color.red)
+                .setTitle('Generator error!')
+                .setDescription(`We don't have \`${args[0]}\` currently!`)
+                .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
+                .setTimestamp()
+            );
+          }
+        } else {
+          return message.channel.send(
+            new MessageEmbed()
+              .setColor(config.color.red)
+              .setTitle('Generator error!')
+              .setDescription(`There is no \`${args[0]}\` service! Make sure you wrote it correctly!`)
+              .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
+              .setTimestamp()
+          );
+        }
+      });
     } else {
-      // If the command executed in another channel
       message.channel.send(
         new MessageEmbed()
           .setColor(config.color.red)
           .setTitle('Wrong command usage!')
-          .setDescription(`This command is unavailable for now!`)
+          .setDescription(`You can't use \`pgen\` command in this channel! Try it in <#${config.pgenChannel}>!`)
           .setFooter(message.author.tag, message.author.displayAvatarURL({ dynamic: true, size: 64 }))
           .setTimestamp()
       );
-    };
+    }
   }
 };
-//Made By ScienceGear#4409 Youtube https://www.youtube.com/c/ScienceGearYT
